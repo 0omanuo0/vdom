7e3a9dff7df82a8d84f02e0c5c334ff2e7fc4a4f
0omanuo0 <manu365manu@gmail.com>
2025-02-21 15:00:26 +0100

    Imrpove to support child components (as variables, not real components)

--- parser/k.js
+++ parser/k.js
@@ -3,43 +3,45 @@ import { h, Component } from "../vdom.js";
 export default function Counter(props, el) {
     return new Component(props, el, function () {
 
-        const [count, setCount] = this.useState(0);
+    const [count, setCount] = this.useState(0);
 
-        function increment() {
-            setCount(count + 1);
-        }
 
-        function decrement() {
-            setCount(count - 1);
-        }
-
-        return (h("template", {}, [
-
-            h("div", {}, [
-
-                h("p", {}, [
-                    "Contador:",
-                    h("span", { id: "counter-value" }, [
-                        count
-                    ])
-                ]),
-                Array.from({ length: count * 100 }, (_, i) =>
-                    h("button", { onclick: increment() }, [
-                        "+",
-                        h("p", {}, [
-                            a
-                        ])
-                    ]),
-                ),
-                h("button", { onclick: decrement() }, [
-                    "-"
-                ]),
-                h("p", {}, [
-                    `Texto: ${text}`
-                ])
-            ])
+    function decrement() {
+        setCount(count - 1);
+    }
+    const Item = (props) => h("div", {}, [
+        
+        h("p", {}, [
+            props.a
         ])
-        );
+    ])
+
+
+    return ( h("div", {}, [
+        
+        h("p", {}, [
+            "Contador:", 
+            h("span", {id: "counter-value"}, [
+                 count
+            ])
+        ]),    
+        Array.from({ length: count * 100 }, (_, i) =>
+            h("button", {onclick: () => setCount(count + 1)}, [
+                "+", 
+                h("p", {}, [
+                    a
+                ])
+            ]),        
+        ) , 
+        h("button", {onclick: decrement()}, [
+            "-"
+        ]),    
+        h("p", {}, [
+            `Texto: ${text}`
+        ]),    
+        Item({a: "hola", b: adios})
+    ])
+    );
 
     });
 }
--- parser/pruebaParser.py
+++ parser/pruebaParser.py
@@ -1,19 +1,8 @@
 from bs4 import BeautifulSoup
 
-# Read the Vue file
-with open("comp.vc", "r", encoding="utf-8") as file:
-    content = file.read()
-
-# Parse using 'html.parser' to handle Vue structure properly
-soup = BeautifulSoup(content, "html.parser")
-
-# Extract template and script separately
-template_content = soup.find("template")
-script_content = soup.find("script")
 
 
-
-def generate_structure(tag, indent=0):
+def generate_structure(tag, indent=0, other=None):
     """ Recursively prints tag structure with indentation 
         ### Limitations:
         - A child can only be a js expression or a text node
@@ -35,7 +24,19 @@ def generate_structure(tag, indent=0):
         # Iterate through children
         for child in tag.children:
             if child.name:
-                t += generate_structure(child, indent + 1)
+                if other is not None and (child.name.capitalize() in other):
+                    # just add the name of the component as: Other({props})
+                    args = child.attrs
+                    parsed_args = {}
+                    for k, v in args.items():
+                        v.strip()
+                        if(v.startswith("{") and v.endswith("}")):
+                            parsed_args[k] = v[1:-1]
+                        else:
+                            parsed_args[k] = "\"" + v + "\""
+                    t += child.name.capitalize() + "({" + ", ".join([f"{k}: {v}" for k, v in parsed_args.items()]) + "}), "
+                else:
+                    t += generate_structure(child, indent + 1, other)
             elif isinstance(child, str):
                 # Remove leading and trailing whitespaces
                 trimmed_child = child.strip()
@@ -80,13 +81,13 @@ def generate_structure(tag, indent=0):
 
 
 
-def parse_template(tc):
-    k = generate_structure(tc, 1)
+def parse_template(tc, other=None):
+    k = generate_structure(tc, 1, other=other)
     # remove the last comma
     k = k[:-1]
     return k
 
-def parse_script(sc, k):
+def parse_script(sc, k, other=None):
     # 1. add string "import { h, Component } from "../vdom.js";" 
     # 2. add a line after the fist \n with: return new Component(props, el, function () {
     # 3. add at the end before the line "}" "return ( " + k + "\n);"
@@ -102,7 +103,17 @@ def parse_script(sc, k):
     
     index = sc.rfind("}") - 1
     k = k.replace("\n", "\n    ")
-    sc = sc[:index] + "\n\n    return ( " + k + "\n    );" + sc[index:]
+    
+    if other is not None:
+        t = "\n    "
+        for key, value in other.items():
+            value = value.replace("\n", "\n    ")
+            t += f"const {key} = (props) => {value}\n"
+        t += "\n\n    return ( " + k + "\n    );"
+        sc = sc[:index] + t + sc[index:]
+        
+    else:
+        sc = sc[:index] + "\n\n    return ( " + k + "\n    );" + sc[index:]
     
     index = sc.rfind("}") - 1
     sc = sc[:index] + "\n\n    });" + sc[index:]
@@ -113,22 +124,50 @@ def parse_script(sc, k):
     
     return sc
     
+component_name = "Counter"
+
+# Read the Vue file
+with open(component_name + ".vc", "r", encoding="utf-8") as file:
+    content = file.read()
+
+# Parse using 'html.parser' to handle Vue structure properly
+soup = BeautifulSoup(content, "html.parser")
+
+# Extract template and script separately
+template_content = soup.find_all("template")
+script_content = soup.find("script")
         
 
 # Print results
-if template_content:
-    # print("Template Section:")
-    # print(template_content.prettify()[:30])
+if template_content:    
+    main_vnode = None
+    other_vnode = {}
     
-    k = parse_template(template_content)
+    main_vnode_parsed = None
+    other_vnode_parsed = {}
     
-    # write it in k.js
+    for a in template_content:
+        name = list(a.attrs.keys())[0].capitalize()
+        a.name = "div"
+        a.attrs = {}
+        if(name == component_name):
+            main_vnode = a
+        else:
+            other_vnode[name] = a
+            
+    
 
-        
+    main_vnode_parsed = parse_template(main_vnode, other=other_vnode)
+    print(main_vnode_parsed)
+    for k, v in other_vnode.items():
+        other_vnode_parsed[k] = parse_template(v)
+        print(other_vnode_parsed[k])
+    
+
     if script_content:
         # print("\nScript Section:")
         # print(script_content.children.__next__())
-        k = parse_script(script_content.children.__next__(), k)
+        k = parse_script(script_content.children.__next__(), main_vnode_parsed, other=other_vnode_parsed)
         
         with open("k.js", "w") as file:
             file.write(k)
